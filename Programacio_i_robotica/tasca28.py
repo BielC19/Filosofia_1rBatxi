6b65e59ecd88586cdb7a0831127599c9f6e2a7cb
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-11-08 00:57:02 +0100

    a

--- Programacio_i_robotica/tasca28.py
+++ Programacio_i_robotica/tasca28.py
@@ -1,10 +1,9 @@
 #Trobar el nombre més gran i també el més petit en una llista donada per l'usuari.
 #Fer-ho amb for primer i després amb while.
-a = 0
+
 llista = []
 b = int(input("Quant numeros vols comprocar: "))
-while a!=b:
+for a in range(b):
     noms = input("Introdueix un numero: ")
     llista.append(noms)
     a += 1
-def maxim(valors):
