68b6496df652a6871c01e492f94f662631363ba1
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-11-14 09:52:08 +0100

    new file:   Programacio_i_robotica/tasca30.py

---
+++ Programacio_i_robotica/tasca30.py
@@ -0,0 +1,15 @@
+#Un nombre x es denomina "perfecte" si la suma dels seus divisors menors que x coincideix precisament amb x. 
+#Exemples de nombres perfectes són el 6 (1 + 2 + 3) i el 28 (1 + 2 + 4 + 7 + 14). 
+#Descobreix quins nombres perfectes n'hi ha que siguin menors que el nombre que introdueixi l'usuari.
+a = int(input("Posa el numero per comprovar si es perfercte: "))
+llista_de_valors=[]
+b = 0
+for i in range(2, a):
+    for n in range(i):
+        n+=1
+        if (i%n) == 0:
+            b += n
+    llista_de_valors.append(b)
+    b=0
+
+print(f"els teus valor perfectes son {llista_de_valors}")
