d536800fc2bc61d72f44cf4b2c180d69612060e1
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-12-16 20:42:17 +0100

    a

--- Programacio_i_robotica/AstroPi/main.py
+++ Programacio_i_robotica/AstroPi/main.py
@@ -1,33 +1,16 @@
-import orbit
-import astro_pi_replay
-import picamera
-import exif
-iss = orbit.ISS
-replay = astro_pi_replay.replay
-PiCamera = picamera.PiCamera
+# Import the PiCamera class from the picamera module
+from picamera import PiCamera
 
-
+# Create an instance of the PiCamera class
 cam = PiCamera()
-cam.resolution = (4056,3040)
 
-def convert(angle):
-    sign, degrees, minutes, seconds = angle.signed_dms()
-    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
-    return sign < 0, exif_angle
+# Set the resolution of the camera to 4056×3040 pixels
+cam.resolution = (4056, 3040)
 
-def custom_capture(iss, camera, image):
-    point = iss.coordinates()
-    south, exif_latitude = convert(point.latitude)
-    west, exif_longitude = convert(point.longitude)
-
-    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
-    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
-    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
-    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-
-    # Instead of capturing an image using the PiCamera object,
-    # use the replay.capture() function to capture a picture using the Astro Pi Replay tool
-    replay.capture()
-
-# Call the custom_capture function with the necessary parameters
-custom_capture(iss(), cam, "gps_image1.jpg")
+# Capture three images using a loop
+for i in range(3):
+    # Capture an image and save it
+    # with a file name like
+    # "image0.png", "image1.png", etc.
+    cam.capture(f"image{i}.png")
+    
