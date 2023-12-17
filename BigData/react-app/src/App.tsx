8450ffc8681a1704347198b90774c8f4da375866
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-12-17 23:32:32 +0100

    A

--- BigData/react-app/src/App.tsx
+++ BigData/react-app/src/App.tsx
@@ -1,18 +1,29 @@
-//Llista
 import React, { useState } from 'react';
 import ListGrup from "./Componets/ListGruoe";
+
+
 function App() {
-  let items = ['Posar una rendadora', 'Fer el deures de BigData', 'Comprar Formatge', 'Netegar la pissara', 'Anara a casa caminant', 'Afagir a Spotify una canço'];
+  const [inputValue, setInputValue] = useState<string>('');
+  const [items, setItems] = useState<string[]>([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    setItems([...items, inputValue]);
+    setInputValue('');
+  };
+
+
+
   const [contratos, setContratos] = useState(items);
 
   const sss = (contrato: string) => {
     let indice = contratos.indexOf(contrato);
-    items.splice(indice, 1); // aplica splice a la copia
+    items.splice(indice,0); // aplica splice a la copia
     setContratos(items)
     console.log(contrato + " Borrat       " + items);
-    };
-    const handleAdds = () => {
-      setContratos([...contratos, newItem]);
     };
 
   const handleSelectItem = (item: string) => {
@@ -20,37 +31,20 @@ function App() {
     console.log(item);
   }
 
-  return <div>
-      <ListGrup handleAdd={handleAdds} items={items} heading="Coses per fer:" AAa={sss} onSelectItem={handleSelectItem}/>
-    </div>
-}
-
-export default App;
-
-//Alert
-/*
-import Alert from "./Componets/Alert";
-function App() {
   return(
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <h1>Coses per fer:</h1>
+      <div className='btnInp'>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        />
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>
+      <ListGrup  items={items} AAa={sss} onSelectItem={handleSelectItem}/>
     </div>
   )
 }
-export default App;
-*/
-//Button
-/*
-import Button from "./Componets/Button"
 
-function App() {
-  return (
-    <div>
-      <Button color="danger" onClick={() => console.log('Clicked')}>Holaa</Button>
-    </div>
-  );
-}
 export default App;
-*/
--- BigData/react-app/src/Componets/ListGruoe.tsx
+++ BigData/react-app/src/Componets/ListGruoe.tsx
@@ -1,35 +1,29 @@
 interface Props {
     items: string[];
-    heading: string;
     onSelectItem: (item: string) => void;
     AAa: (item: string) =>void;
-    handleAdd: (event: React.FormEvent) => void;
 }
 import { useState } from "react";
 
-function ListGrup({items, heading, onSelectItem, AAa, handleAdd}: Props) {
+function ListGrup({items, onSelectItem, AAa}: Props) {
 // Hook : vol dir que te dades que poden canviar tota l'estona
 const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-        <h1>{heading}</h1>
-        <form >
-        <input type="text" placeholder="Introdueix el que has de fer" />
-        <button onClick={handleAdd} type="submit">Afagir</button>
-        </form>
-        {items.length === 0 && <p>Items not found</p>} 
+        
         <ul className="list-group">
+            <li className={ 'list-group-item' } ></li>
+            {items.length === 0 && <p>Items not found</p>} 
             {items.map((item, index) => (
             <li 
                 className={ /*selectedIndex === index ? 'list-group-item ': */'list-group-item'} 
                 key={item} 
                 onClick={() => {
                     setSelectedIndex(index);
-                    /*onSelectItem(item)*/
-                    AAa(item);
+                    onSelectItem(item)
                 }} 
-            >
+            ><input type="checkbox" name="index" onChange={() => AAa(item) } />
             {item}
                 </li>
             ))}
