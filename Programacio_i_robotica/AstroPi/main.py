3ff3be94af95f70c3a05b9b42a643da30e163e73
BielC19 <118078513+BielC19@users.noreply.github.com>
2024-01-10 08:16:39 +0100

    a

--- Programacio_i_robotica/AstroPi/main.py
+++ Programacio_i_robotica/AstroPi/main.py
@@ -1,55 +1,120 @@
-'''
-# Import the PiCamera class from the picamera module
 from picamera import PiCamera
+from orbit import ISS
+from exif import Image
+from datetime import datetime
+import cv2
+import math
 
-# Create an instance of the PiCamera class
 cam = PiCamera()
-
-# Set the resolution of the camera to 4056×3040 pixels
 cam.resolution = (4056, 3040)
 
-# Capture three images using a loop
-for i in range(3):
-    # Capture an image and save it
-    # with a file name like
-    # "image0.png", "image1.png", etc.
-    cam.capture(f"image{i}.png")
-'''
-from orbit import ISS
-from picamera import PiCamera
+image_1 = 'photo_0683.jpg'
+image_2 = 'photo_0684.jpg'
 
-cam = PiCamera()
-cam.resolution = (4056,3040)
+estimate_kmps = 7.1234567890
+estimate_kmps_formatted = "{:.4f}".format(estimate_kmps)
+output_string = estimate_kmps_formatted
+file_path = "result.txt"  # Replace with your desired file path
+
+
+def get_time(image):
+    with open(image, 'rb') as image_file:
+        img = Image(image_file)
+        time_str = img.get("datetime_original")
+        time = datetime.strptime(time_str, '%Y:%m:%d %H:%M:%S')
+    return time
+
+def get_time_difference(image_1, image_2):
+    time_1 = get_time(image_1)
+    time_2 = get_time(image_2)
+
+    time_difference = time_2 - time_1
+    print(time_difference.seconds)
+    return time_difference.seconds
+
+
+def convert_to_cv(image_1, image_2):
+    image_1_cv = cv2.imread(image_1, 0)
+    image_2_cv = cv2.imread(image_2, 0)
+
+    return image_1_cv, image_2_cv
+
+def calculate_features(image_1, image_2, feature_number):
+    orb = cv2.ORB_create(nfeatures = feature_number)
+    keypoints_1, descriptors_1 = orb.detectAndCompute(image_1_cv, None)
+    keypoints_2, descriptors_2 = orb.detectAndCompute(image_2_cv, None)
+
+    return keypoints_1, keypoints_2, descriptors_1, descriptors_2
+
+def calculate_matches(descriptors_1, descriptors_2):
+    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = brute_force.match(descriptors_1, descriptors_2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    return matches
+
+def display_matches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches):
+    match_img = cv2.drawMatches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches[:100], None)
+    resize = cv2.resize(match_img, (1600,600), interpolation = cv2.INTER_AREA)
+    cv2.imshow('matches', resize)
+    cv2.waitKey(0)
+    cv2.destroyWindow('matches')
+
+def find_matching_coordinates(keypoints_1, keypoints_2, matches):
+    coordinates_1 = []
+    coordinates_2 = []
+
+    for match in matches:
+        image_1_idx = match.queryIdx
+        image_2_idx = match.trainIdx
+
+        (x1,y1) = keypoints_1[image_1_idx].pt
+        (x2,y2) = keypoints_2[image_2_idx].pt
+
+        coordinates_1.append((x1,y1))
+        coordinates_2.append((x2,y2))
+
+    return coordinates_1, coordinates_2
+
+def calculate_mean_distance(coordinates_1, coordinates_2):
+    all_distances = 0
+    merged_coordinates = list(zip(coordinates_1, coordinates_2))
+    for coordinate in merged_coordinates:
+        x_difference = coordinate[0][0] - coordinate[1][0]
+        y_difference = coordinate[0][1] - coordinate[1][1]
+        distance = math.hypot(x_difference, y_difference)
+        all_distances = all_distances + distance
+    return all_distances / len(merged_coordinates)
+
+def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
+    distance = feature_distance * GSD / 100000
+    speed = distance / time_difference
+    return speed
+
+
 
 def convert(angle):
-
-    # Convert a `skyfield` Angle to an Exif-appropriate
-    # representation (positive rationals)
-    # e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
-
-    # Return a tuple containing a Boolean and the converted angle,
-    # with the Boolean indicating if the angle is negative
-
     sign, degrees, minutes, seconds = angle.signed_dms()
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
 
 def custom_capture(iss, camera, image):
-    # Use `camera` to capture an `image` file with lat/long Exif data
     point = iss.coordinates()
-
-    # Convert the latitude and longitude to Exif-appropriate
-    # representations
     south, exif_latitude = convert(point.latitude)
     west, exif_longitude = convert(point.longitude)
-
-    # Set the Exif tags specifying the current location
     camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
     camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
     camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
     camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
-
-    # Capture the image
     camera.capture(image)
 
-custom_capture(ISS(), cam, "gps_image1.jpg")
+  # Replace with your estimate
+
+# Format the estimate_kmps to have a precision
+# of 5 significant figures
+
+# Create a string to write to the file
+with open(file_path, 'w') as file:
+    file.write(output_string)
+
+print("Data written to", file_path)
+custom_capture(ISS(), cam, {"gps_image" + {i} + ".jpg"})
