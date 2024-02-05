af51a5be0f3d53251166b0d48fc52824908ee038
Bielc <costasamso@gmail.com>
2024-02-05 09:36:48 +0100

    dia2.py

--- Programacio_i_robotica/microPi/dia2.py
+++ Programacio_i_robotica/microPi/dia2.py
@@ -1,12 +1,18 @@
 from machine import Pin
 import time
 
-led = Pin(10, Pin.OUT)
+led1 = Pin(10, Pin.OUT)
 led2 = Pin(11, Pin.OUT)
 led3 = Pin(12, Pin.OUT)
 
 while True:
-    led.toggle()
+    led1.toggle()
+    time.sleep(0.2)
+    led1.toggle()
+    led2.toggle()
+    time.sleep(0.2)
     led2.toggle()
     led3.toggle()
-    time.sleep(1)
+    time.sleep(0.2)
+    led3.toggle()
+    time.sleep(0.2)
