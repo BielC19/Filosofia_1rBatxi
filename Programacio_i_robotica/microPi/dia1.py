cd20148648805f9f68a29318b496ebab46948cab
Bielc <costasamso@gmail.com>
2024-02-05 09:14:19 +0100

    dia1.py

--- Programacio_i_robotica/microPi/dia1.py
+++ Programacio_i_robotica/microPi/dia1.py
@@ -2,11 +2,8 @@ from machine import Pin
 import time
 
 led = Pin(10, Pin.OUT)
-led2 = Pin(11, Pin.OUT)
-led3 = Pin(12, Pin.OUT)
+
 
 while True:
     led.toggle()
-    led2.toggle()
-    led3.toggle()
     time.sleep(1)
