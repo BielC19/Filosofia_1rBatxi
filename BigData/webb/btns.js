08471b7a4edfbe4ef35cbe47bf4fbf9e8561aebc
Bielc <costasamso@gmail.com>
2024-02-21 11:58:45 +0100

    holla

--- BigData/webb/btns.js
+++ BigData/webb/btns.js
@@ -28,7 +28,7 @@ window.onload = function() {
 
         nouA.setAttribute('id', lletres2[i])
         nouA.setAttribute('target', '_top');
-        nouA.setAttribute('href', dicA[i] + '.html');
+        nouA.setAttribute('href', dicA[i]);
         ///nouA.setAttribute('href', dicA[i] + '.html');
         nouA.innerHTML = dicA[i];
         if (url === '/1rBarxiA_BCS/BigData/webb/' + dicA) {
