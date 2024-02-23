b17bb408a0fbf895c867fe617092dbe5f29a780e
Bielc <costasamso@gmail.com>
2024-02-23 13:39:10 +0100

    des del comando

--- BigData/webb/copy.js
+++ BigData/webb/copy.js
@@ -1,3 +1,22 @@
 function copiarAlPortapapeles(id_elemento) {
-    window.open('https://mail.google.com/mail/?view=cm&amp;fs=1&amp;to=bielcostasmso%40insalba.cat&amp;authuser=0')
+    console.log(id_elemento)
+    navigator.clipboard.writeText('Hola mundo')
+
+    if (id_elemento == 'email1') {
+        navigator.clipboard.writeText('bielcostasamso@insalba.cat')
+
+    }
+    else if (id_elemento == 'email2') {
+        navigator.clipboard.writeText('gbanoslopez@insalba.cat')
+
+    }
+    else if (id_elemento == 'email3') {
+        navigator.clipboard.writeText('hmartinlozano@insalba.cat')
+
+    }
+
+    window.alert('Copiat! \n Ja pots enganxar el correu i contactarnos')
+    
+    window.open('https://mail.google.com/mail/?view=cm&amp;fs=1&amp;to=bielcostasmso%40insalba.cat&amp;authuser=0');
+    
 }
--- Programacio_i_robotica/microPi/dia6.py
+++ Programacio_i_robotica/microPi/dia6.py
@@ -1,22 +1,19 @@
 from machine import Pin, PWM
-import utime
+import time
 
+#Configura el Servo de 360
+servo_360 = PWM(Pin(8))
+servo_360.freq(50)
 
-def main():
+pulsador = Pin(12, Pin.IN)
 
-    
-    #Configura el Servo de 360
-    servo_360 = PWM(Pin(8))
-    servo_360.freq(50)
-    
-    pulsador = Pin(12, Pin.IN)
-    
-    while True:
-        if pulsador == 0:
-            angulo = float(input('Ingrese un ángulo: '))
+while True:
+    print(pulsador.value())
+    time.sleep(0.1)
+    if pulsador.value() == 0:
+        angulo = float(input('Ingrese un ángulo: '))
+        if angulo >= 0 and angulo <= 190:
+            if angulo == 45:
+                print("Calibrat")
             duty = int((12.346*angulo**2 + 7777.8*angulo + 700000))
             servo_360.duty_ns(duty)
-    
-    
-if __name__ == '__main__':
-    main()
