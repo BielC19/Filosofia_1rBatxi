1b8d4a11aa399917d3fa7ed4c4bbafae1867d01e
Bielc <costasamso@gmail.com>
2024-03-17 02:20:06 +0100

    des del comando

--- Filo/maual/poster/QiA.js
+++ Filo/maual/poster/QiA.js
@@ -9,9 +9,43 @@ function customInputAlert() {
     correcte.style.display = 'flex';
 };
 
+
 function tancarA() {
     correcte.style.display = 'none';
+    alert('Moltes gracies, has passat la prova amb ' + textosos)
+    setTimeout(() => window.close('','_top',''), 1000);
 }
+const options = document.querySelectorAll('.option');
+const result = document.getElementById('result');
+const intents = document.getElementById('tryesses');
+
+let proves = 0;
+let textosos = proves + ' intents'
+
+options.forEach(option => {
+	option.addEventListener('click', () => {
+		options.forEach(o => o.disabled = false);
+		if (option.dataset.correct === 'true') {
+			result.textContent = 'Correct!';
+			result.style.color = '#0f0';
+		} else {
+			result.textContent = 'Incorrect!';
+			result.style.color = '#f00';
+			option.classList.add('incorrect');
+			setTimeout(() => option.classList.remove('incorrect'), 500);
+		}
+        proves += 1;
+        if (proves==1) {
+            textosos = proves + ' intent'
+        }
+        else{
+            textosos = proves + ' intents'
+        }
+        intents.textContent = 'portes ' + textosos
+    });
+});
+
+/*
 const ball = document.getElementById('Wr1');
 
 const screenHeight = screen.availHeight;
@@ -22,14 +56,10 @@ function animateBall() {
     ballTop -= 5;
     for (let index = 0; index < 2; index++) {
         if (ballTop < 0) {
-            ballTop = screenHeight - ;
+            ballTop = screenHeight - 2;
         }
         ball.style.top = ballTop + 'px';
-        requestAnimationFrame(animateBall);
-        setTimeout(1000)
+        
     }
-
-
-}
-
-animateBall();
+    ball.style.position = 'relative'
+}*/
