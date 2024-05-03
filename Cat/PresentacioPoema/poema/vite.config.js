a43550c13fe20e6e53f2f7d005c89b7f2a958dd3
BielC19 <costasamso@gmail.com>
2024-05-03 13:26:57 +0200

    modified:   Cat/PresentacioPoema/poema/package-lock.json 	modified:   Cat/PresentacioPoema/poema/package.json 	modified:   Cat/PresentacioPoema/poema/vite.config.js

--- Cat/PresentacioPoema/poema/vite.config.js
+++ Cat/PresentacioPoema/poema/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/1rBarxiA_BCS/Cat/PresentacioPoema/poema",
   plugins: [react()],
 })
